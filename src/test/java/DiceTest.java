f2564a2a0431a42a46b27cb95bd7d5a0c73ae562
WeiYi Yu <s3811924@student.rmit.edu.au>
2019-08-25 19:09:56 +1000

    Added DiceTest

---
+++ src/test/java/DiceTest.java
@@ -0,0 +1,27 @@
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author WeiYi Yu
+ * @date 2019-08-24
+ */
+public class DiceTest {
+
+    private Dice dice;
+
+    @Before
+    public void setUp() throws Exception {
+        dice = new Dice();
+    }
+
+    @Test
+    public void testRollRange() throws Exception {
+        // run a few times to make sure that number doesn't exceed the maximum/minimum.
+        for (int i = 0; i < 50; i++) {
+            int num = dice.roll();
+            if (num < Dice.DICE_NUM_MIN || num > Dice.DICE_NUM_MAX) {
+                throw new Exception();
+            }
+        }
+    }
+}
