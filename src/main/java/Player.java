62c78fb166010b424ca4d452e92d2e3f16f58dda
WeiYi Yu <s3811924@student.rmit.edu.au>
2019-08-26 18:53:41 +1000

    Added mock Player class

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -3,4 +3,13 @@
  * @date 2019-08-25
  */
 public class Player {
+    private String name;
+
+    public Player(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
 }
